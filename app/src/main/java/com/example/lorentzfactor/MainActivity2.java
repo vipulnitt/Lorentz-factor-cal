53fccb7c7c6cca0be6f3e7e3707b7773b43c86fc
Vipul <vp98504@gmail.com>
2021-06-18 19:35:10 +0530

    Level 3

--- app/src/main/java/com/example/lorentzfactor/MainActivity2.java
+++ app/src/main/java/com/example/lorentzfactor/MainActivity2.java
@@ -44,8 +44,8 @@ public class MainActivity2 extends AppCompatActivity {
                     {
                         double d = Math.pow(Math.pow(300000000, 2) - Math.pow(x, 2), 0.5);
                         double z = 300000000 / d;
-                       double m = Math.round(z*100000);
-                       m=m/100000;
+                       double m = Math.round(z*10000000);
+                       m=m/10000000;
                         if(m==y){
                             view.setBackgroundResource(R.color.Blue);
                             textView.setText("Correct Answer!");
